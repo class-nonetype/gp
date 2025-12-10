f198722600e9258cd4114b23bb04fa0e45a471b2
Gonzalo Vivanco Zepeda <Gonzalo Vivanco Zepeda>
2025-12-09 23:21:43 -0300

    ...

---
+++ server/src/core/database/models/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from .user_accounts import UserAccounts
+from .user_profiles import UserProfiles
+from .tickets import Tickets
+from .ticket_attachments import TicketAttachments
+from .types import *
+from .user_groups import UserGroups
+
+
+
+__all__ = (
+    "UserAccounts",
+    "UserProfiles",
+    "Tickets",
+    "TicketAttachments",
+    "RequestTypes",
+    "PriorityTypes",
+    "StatusTypes",
+    "UserGroups"
+)
